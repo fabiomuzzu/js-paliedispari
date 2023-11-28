a82467c9ac9126491b85d4f9a6262b94cb6d9e53
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-28 17:35:08 +0100

    added random number function

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -11,3 +11,19 @@ while (ask == 'pari' || ask == 'dispari' ) {
     ask++;
 } 
 
+// COME FACCIO A RIPETERE IL PROMPT NEL CICLO WHILE SE INSERIMENTO NON CORRETTO?
+
+// 4 - Creo prompt per far scegliere numero da 1 a 5 all'utente
+let player_num = prompt('Scegli un numero da 1 a 5');
+console.log(player_num);
+
+// STESSO PROBLEMA DI SU
+
+// 5 - Creo una funzione che genera un numero random da 1 a 5 per il computer
+function randomNumber(){
+    pc_num = Math.floor(Math.random() * 5) + 1;
+    console.log(pc_num);
+    return pc_num;
+}
+
+randomNumber();
