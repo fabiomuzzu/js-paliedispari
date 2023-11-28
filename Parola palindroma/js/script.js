9516b669646b1bb510fb291a3c64663d8aa4c863
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-28 18:19:58 +0100

    fixed if/else excercise 1

--- Parola palindroma/js/script.js
+++ Parola palindroma/js/script.js
@@ -3,24 +3,22 @@ let word = prompt('Inserisci una parola');
 console.log(word);
 
 // 2 - Creare funzione per scomporre la parola
-let reverse_word;
 function wordLetters(word){
-    console.log(word.split('').reverse().join('').toLowerCase());
-    reverse_word = (word.split('').reverse().join('').toLowerCase());
+    word = word.toLowerCase();
+    let reverse_word = word.split('').reverse().join('').toLowerCase();
     console.log(`la reverse word è: ${reverse_word}`);
 
-    
     // 4 - Creo ciclo if/else per visualizzare in console il risultato
-    if (reverse_word.toLowerCase == word.toLowerCase){
-        console.log('La parola è palindroma!')
+    if (reverse_word == word){
+        console.log('La parola è palindroma!');
     }
     else{
-        console.log('La parola NON è palindroma!')
+        console.log('La parola NON è palindroma!');
     }
     return reverse_word;
 }
 
 
 // 3 - Richiamo la funzione
-wordLetters(word);
+let reverse_word = wordLetters(word);
 
